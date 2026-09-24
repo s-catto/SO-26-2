a339064a984e0464040b7baaea703ede68e6b4f3
sofi <sb.candiotto@gmail.com>
2026-09-23 22:32:26 -0300

    correção prioridades dnamicas negativas

--- ppos-aluno/kernel/scheduler.c
+++ ppos-aluno/kernel/scheduler.c
@@ -48,7 +48,7 @@ struct task_t *scheduler(struct queue_t *ready_queue)
     // envelhece as outras tasks
     task = queue_head(ready_queue);
     while (task != NULL) {
-        if (task->priod >= 0)
+        if (task->priod >= -20)
             task->priod = task->priod - 1;
 
         task = queue_next(ready_queue);
